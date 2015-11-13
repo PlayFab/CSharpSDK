1b5143e29e010e1851a4518b6636965c97f2e764
pgilmorepf <paul@playfab.com>
2015-11-13 11:32:10 -0800

    Removing apiVersion, converting everything to use sdkVersion

--- PlayFabServerSDK/source/PlayFabVersion.cs
+++ PlayFabServerSDK/source/PlayFabVersion.cs
@@ -3,12 +3,11 @@ namespace PlayFab.Internal
 {
 	public class PlayFabVersion
 	{
-		public static string ApiRevision = "1.9.20151109";
 		public static string SdkRevision = "0.9.151109";
 
 		public static string getVersionString()
 		{
-			return "CSharpSDK-" + SdkRevision + "-" + ApiRevision;
+			return "CSharpSDK-" + SdkRevision;
 		}
 	}
 }
