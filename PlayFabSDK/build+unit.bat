e43d7fe60b828ea98cd2bbc6e73e70ecc9874a4d
pgilmorepf <paul@playfab.com>
2015-11-13 17:10:41 -0800

    Revising the build scripts in CSharp for Jenkins.

--- PlayFabSDK/build+unit.bat
+++ PlayFabSDK/build+unit.bat
@@ -1,12 +1,4 @@
-cd C:\depot\sdks\CSharpSDK\PlayFabSDK
-
-IF DEFINED VisualStudioVersion GOTO VSSetup
-call "%PROGRAMFILES(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-:VSSetup
-
 msbuild PlayFabSDK.csproj /p:configuration="Debug" /t:Rebuild
 msbuild PlayFabSDK.csproj /p:configuration="Release" /t:Rebuild
 msbuild UnittestRunner/UnittestRunner.csproj /p:configuration="Debug" /t:Rebuild
 msbuild UnittestRunner/UnittestRunner.csproj /p:configuration="Release" /t:Rebuild
-
-rd obj /s /q
