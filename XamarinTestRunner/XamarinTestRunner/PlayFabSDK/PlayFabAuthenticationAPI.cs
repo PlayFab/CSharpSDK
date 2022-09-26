a247f485a873cab2224461a2f4906ea25ffdd09f
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-09-26 21:16:23 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220926

--- XamarinTestRunner/XamarinTestRunner/PlayFabSDK/PlayFabAuthenticationAPI.cs
+++ XamarinTestRunner/XamarinTestRunner/PlayFabSDK/PlayFabAuthenticationAPI.cs
@@ -38,6 +38,33 @@ namespace PlayFab
         }
 
         /// <summary>
+        /// Create a game_server entity token and return a new or existing game_server entity.
+        /// </summary>
+        public static async Task<PlayFabResult<AuthenticateCustomIdResult>> AuthenticateGameServerWithCustomIdAsync(AuthenticateCustomIdRequest request, object customData = null, Dictionary<string, string> extraHeaders = null)
+        {
+            await new PlayFabUtil.SynchronizationContextRemover();
+
+            var requestContext = request?.AuthenticationContext ?? PlayFabSettings.staticPlayer;
+            var requestSettings = PlayFabSettings.staticSettings;
+            if (requestContext.EntityToken == null) throw new PlayFabException(PlayFabExceptionCode.EntityTokenNotSet, "Must call Client Login or GetEntityToken before calling this method");
+
+
+            var httpResult = await PlayFabHttp.DoPost("/GameServerIdentity/AuthenticateGameServerWithCustomId", request, "X-EntityToken", requestContext.EntityToken, extraHeaders);
+            if (httpResult is PlayFabError)
+            {
+                var error = (PlayFabError)httpResult;
+                PlayFabSettings.GlobalErrorHandler?.Invoke(error);
+                return new PlayFabResult<AuthenticateCustomIdResult> { Error = error, CustomData = customData };
+            }
+
+            var resultRawJson = (string)httpResult;
+            var resultData = PluginManager.GetPlugin<ISerializerPlugin>(PluginContract.PlayFab_Serializer).DeserializeObject<PlayFabJsonSuccess<AuthenticateCustomIdResult>>(resultRawJson);
+            var result = resultData.data;
+
+            return new PlayFabResult<AuthenticateCustomIdResult> { Result = result, CustomData = customData };
+        }
+
+        /// <summary>
         /// Delete a game_server entity.
         /// </summary>
         public static async Task<PlayFabResult<EmptyResponse>> DeleteAsync(DeleteRequest request, object customData = null, Dictionary<string, string> extraHeaders = null)
