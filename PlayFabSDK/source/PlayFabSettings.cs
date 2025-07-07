befbc7c1d4271d0651d28818c439a643887ae24f
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2025-07-07 20:03:58 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#250704

--- PlayFabSDK/source/PlayFabSettings.cs
+++ PlayFabSDK/source/PlayFabSettings.cs
@@ -6,9 +6,9 @@ namespace PlayFab
 {
     public class PlayFabSettings
     {
-        public const string SdkVersion = "1.203.250620";
-        public const string BuildIdentifier = "adobuild_csharpsdk_117";
-        public const string SdkVersionString = "CSharpSDK-1.203.250620";
+        public const string SdkVersion = "1.204.250704";
+        public const string BuildIdentifier = "adobuild_csharpsdk_118";
+        public const string SdkVersionString = "CSharpSDK-1.204.250704";
         /// <summary> This is only for customers running a private cluster.  Generally you shouldn't touch this </summary>
         public static string DefaultProductionEnvironmentUrl = "playfabapi.com";
 
